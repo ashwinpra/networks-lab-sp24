9d22e814ad77e465a3d52d3ca55f0844fa6edd49
ashwinpra <ashprasanth2003@gmail.com>
2024-01-08 16:32:35 +0530

    A1 B done?

---
+++ A1/test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char const *argv[])
+{
+    FILE* fp = fopen("test.txt", "w");
+
+    char* hello = "Hello World";
+    
+    return 0;
+}
