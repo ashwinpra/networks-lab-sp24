6f23b8249f238bf650479318a107f18996508f85
ashwinpra <ashprasanth2003@gmail.com>
2024-02-09 11:51:17 +0530

    i think this is working

--- A3/mailclient.c
+++ A3/mailclient.c
@@ -87,6 +87,8 @@ int main(int argc, char const *argv[])
             strcat(buf, username);
             send_message(sockfd, buf);
 
+            printf("USER %s\n", username);
+
             // expected: +OK
             if(!receive_pop3_status(sockfd)) continue;
 
@@ -461,35 +463,52 @@ void get_mail_from_server(int sockfd, char mail[(MAX_LINES+3)*(MAX_LINE_LEN+1)+1
                     temp[temp_index++]=buf[buf_index++];
                     temp[temp_index++]='\0';
                     line++;
+
+                    if(line==1){
+                        // expected: +OK
+                        if(strncmp(temp, "+OK", 3) != 0) {
+                            printf("Error in managing mail: %s\n", temp);
+                            send_message(sockfd, "QUIT\r\n");
+                            return;
+                        }
+                    }
+                    else if(strcmp(temp,".\r\n")==0){
+                        temp_index=0;
+                        bzero(temp,100);
+                        done=1;
+                        break;
+                    }
+                    if(line!=1) strcat(mail,temp);
+                    temp_index=0;
+                    bzero(temp,100);
                 }
             }
             else if(temp_index!=0 && (buf[buf_index]=='\n') && (temp[temp_index-1]=='\r')){
                     temp[temp_index++]=buf[buf_index++];
                     temp[temp_index++]='\0';
                     line++;
+                    
+                    if(line==1){
+                        // expected: +OK
+                        if(strncmp(temp, "+OK", 3) != 0) {
+                            printf("Error in managing mail: %s\n", temp);
+                            send_message(sockfd, "QUIT\r\n");
+                            return;
+                        }
+                    }
+                    else if(strcmp(temp,".\r\n")==0){
+                        temp_index=0;
+                        bzero(temp,100);
+                        done=1;
+                        break;
+                    }
+                    if(line!=1) strcat(mail,temp);
+                    temp_index=0;
+                    bzero(temp,100);
             }
             else{
                 temp[temp_index++]=buf[buf_index++];
-                continue;
             }
-            
-            if(line==1){
-                // expected: +OK
-                if(strncmp(temp, "+OK", 3) != 0) {
-                    printf("%s\n", temp);
-                    send_message(sockfd, "QUIT\r\n");
-                    return;
-                }
-            }
-            else if(strcmp(temp,".\r\n")==0){
-                temp_index=0;
-                bzero(temp,100);
-                done=1;
-                break;
-            }
-            if(line!=1) strcat(mail,temp);
-            temp_index=0;
-            bzero(temp,100);
         }
     }
 }
@@ -511,6 +530,7 @@ void get_maillist_from_server(int sockfd, int num_mails, char* mails[MAX_LINES+3
 
         // server will send line-by-line until .CRLF
         // from it, extract mail in "From: " line, Time of mail, Subject
+        //receive mail from client
             int line=0;
             bzero(temp,100);
             int temp_index=0,buf_index=0,done=0;
@@ -530,53 +550,86 @@ void get_maillist_from_server(int sockfd, int num_mails, char* mails[MAX_LINES+3
                             temp[temp_index++]=buf[buf_index++];
                             temp[temp_index++]='\0';
                             line++;
+
+                            if(line==1){
+                                // expected: +OK
+                                if(strncmp(temp, "+OK", 3) != 0) {
+                                    printf("Error in managing mail: %s\n", temp);
+                                    send_message(sockfd, "QUIT\r\n");
+                                    return;
+                                }
+                            }
+                            if(line==2){
+                                // sender comes after "From: "
+                                strcpy(sender, temp+6);
+                                remove_CRLF(sender);
+                                strip(sender);
+                            }
+                            else if(line==4){
+                                // subject comes after "Subject: "
+                                strcpy(subject, temp+9);
+                                remove_CRLF(subject);
+                                strip(subject);
+                            }
+                            else if(line==5){
+                                // time comes after "Received: "
+                                strcpy(time, temp+10);
+                                remove_CRLF(time);
+                                strip(time);
+                            }
+                            else if(strcmp(temp,".\r\n")==0){
+                                temp_index=0;
+                                bzero(temp,100);
+                                done=1;
+                                break;
+                            }
+                            temp_index=0;
+                            bzero(temp,100);
                         }
                     }
                     else if(temp_index!=0 && (buf[buf_index]=='\n') && (temp[temp_index-1]=='\r')){
                             temp[temp_index++]=buf[buf_index++];
                             temp[temp_index++]='\0';
                             line++;
+                            
+                            if(line==1){
+                                // expected: +OK
+                                if(strncmp(temp, "+OK", 3) != 0) {
+                                    printf("Error in managing mail: %s\n", temp);
+                                    send_message(sockfd, "QUIT\r\n");
+                                    return;
+                                }
+                            }
+                            if(line==2){
+                                // sender comes after "From: "
+                                strcpy(sender, temp+6);
+                                remove_CRLF(sender);
+                                strip(sender);
+                            }
+                            else if(line==4){
+                                // subject comes after "Subject: "
+                                strcpy(subject, temp+9);
+                                remove_CRLF(subject);
+                                strip(subject);
+                            }
+                            else if(line==5){
+                                // time comes after "Received: "
+                                strcpy(time, temp+10);
+                                remove_CRLF(time);
+                                strip(time);
+                            }
+                            else if(strcmp(temp,".\r\n")==0){
+                                temp_index=0;
+                                bzero(temp,100);
+                                done=1;
+                                break;
+                            }
+                            temp_index=0;
+                            bzero(temp,100);
                     }
                     else{
                         temp[temp_index++]=buf[buf_index++];
-                        continue;
                     }
-
-
-                    if(line==1){
-                        // expected: +OK
-                        if(strncmp(temp, "+OK", 3) != 0) {
-                            printf("%s\n", temp);
-                            send_message(sockfd, "QUIT\r\n");
-                            return;
-                        }
-                    }
-                    if(line==2){
-                        // sender comes after "From: "
-                        strcpy(sender, temp+6);
-                        remove_CRLF(sender);
-                        strip(sender);
-                    }
-                    else if(line==4){
-                        // subject comes after "Subject: "
-                        strcpy(subject, temp+9);
-                        remove_CRLF(subject);
-                        strip(subject);
-                    }
-                    else if(line==5){
-                        // time comes after "Received: "
-                        strcpy(time, temp+10);
-                        remove_CRLF(time);
-                        strip(time);
-                    }
-                    else if(strcmp(temp,".\r\n")==0){
-                        temp_index=0;
-                        bzero(temp,100);
-                        done=1;
-                        break;
-                    }
-                    temp_index=0;
-                    bzero(temp,100);
                 }
             }
 
@@ -585,14 +638,16 @@ void get_maillist_from_server(int sockfd, int num_mails, char* mails[MAX_LINES+3
         sprintf(mail_num, "%d", i);
         mails[i-1] = malloc(strlen(mail_num)+strlen(sender)+strlen(time)+strlen(subject)+6);
         strcpy(mails[i-1], mail_num);
-        strcat(mails[i-1], "\t<");
+        strcat(mails[i-1], " <");
         strcat(mails[i-1], sender);
-        strcat(mails[i-1], ">\t");
+        strcat(mails[i-1], "> ");
         strcat(mails[i-1], time);
-        strcat(mails[i-1], "\t");
+        strcat(mails[i-1], " ");
         strcat(mails[i-1], subject);
         }
     }
+               
+
 
 int receive_smtp_status(int sockfd, int expected) {
     char full_msg[1000];
